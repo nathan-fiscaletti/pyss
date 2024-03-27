1ce0cb8c9d50c19037fe54bed98287404ec93654
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2024-03-27 00:49:22 -0500

    v1.0.4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="pyss",
-    version="1.0.3",
+    version="1.0.4",
     description="Adds a utility for running pyss.yaml files.",
     author="Nathan Fiscaletti",
     author_email="nate.fiscaletti@gmail.com",
     packages=["pyss"],
-    install_requires=["termcolor", "PyYAML"],
+    install_requires=["termcolor==2.2.0", "PyYAML==6.0"],
     entry_points={
         "console_scripts": [
             "pyss = pyss.main:main",
